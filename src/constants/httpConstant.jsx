97431ef22bef80e8fa54ff56b48404c7597708d9
asiedujosh <asiedujosh@yahoo.com>
2024-07-16 22:51:59 +0000

    http Issue fixing

--- src/constants/httpConstant.jsx
+++ src/constants/httpConstant.jsx
@@ -1,6 +1,6 @@
 export const TIMEOUT = { timeout: 150000 }
-// export const URL = process.env.API_URL
-export const URL = "http://127.0.0.1:8000"
+export const URL = process.env.API_URL
+// export const URL = "http://127.0.0.1:8000"
 
 //System Responses
 
